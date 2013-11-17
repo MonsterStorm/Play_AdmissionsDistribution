5f5539fc1918273c2a9a11bbbcf70bd4e9068e48
cst <cst8588092@yahoo.com.cn>
2013-11-17 21:39:57 +0800

    Signed-off-by: cst <cst8588092@yahoo.com.cn>

--- public/javascripts/cst.js
+++ public/javascripts/cst.js
@@ -1,8 +1,93 @@
+function HashMap() {
+	/** Map 大小 * */
+	var size = 0;
+	/** 对象 * */
+	var entry = new Object();
+
+	/** 存 * */
+	this.put = function(key, value) {
+		if (!this.containsKey(key)) {
+			size++;
+		}
+		entry[key] = value;
+	}
+
+	/** 取 * */
+	this.get = function(key) {
+		if (this.containsKey(key)) {
+			return entry[key];
+		} else {
+			return null;
+		}
+	}
+
+	/** 删除 * */
+	this.remove = function(key) {
+		if (delete entry[key]) {
+			size--;
+		}
+	}
+
+	/** 是否包含 Key * */
+	this.containsKey = function(key) {
+		return (key in entry);
+	}
+
+	/** 是否包含 Value * */
+	this.containsValue = function(value) {
+		for ( var prop in entry) {
+			if (entry[prop] == value) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/** 所有 Value * */
+	this.values = function() {
+		var values = new Array(size);
+		for ( var prop in entry) {
+			values.push(entry[prop]);
+		}
+		return values;
+	}
+
+	/** 所有 Key * */
+	this.keys = function() {
+		var keys = new Array(size);
+		for ( var prop in entry) {
+			keys.push(prop);
+		}
+		return keys;
+	}
+
+	/** Map Size * */
+	this.size = function() {
+		return size;
+	}
+}
+
 function pushState(li, domain, url, data) {
 	if (!history.urls) {
 		history.urls = [];// array
+		history.maps = new HashMap();
 	}
-	
+
+	var key = domain + url;
+	if(history.maps.containsKey(key)) {
+		var index = history.maps.get(key);
+		//console.dir(key + "===" + index);
+		history.urls.splice(index, 1);//删除一个元素
+		
+		var i = index;
+		for(; i < history.urls.length; i++){//更新所有的index
+			var tkey = history.urls[i].domain + history.urls[i].url;
+			var tindex = history.maps.get(tkey);
+			history.maps.put(tkey, tindex - 1);
+		}
+	}
+	//console.dir("++++++++++++++++++++++++");
+	//console.dir(history.urls.length);
 	history.urls.push({
 		li : li,
 		domain : domain,
@@ -10,6 +95,10 @@ function pushState(li, domain, url, data) {
 		data : data
 	});
 	history.index = history.urls.length - 1;
+	history.maps.put(key, history.index);
+	//console.dir(key + "===" + history.maps.get(key));
+	//console.dir(history.urls);
+	//console.dir(history.index);
 }
 
 function preState() {
@@ -24,6 +113,7 @@ function preState() {
 		var url = history.urls[history.index].url;
 		var data = history.urls[history.index].data;
 		getPage(li, domain, url, data, true);
+
 	}
 }
 
@@ -39,5 +129,6 @@ function nextState() {
 		var url = history.urls[history.index].url;
 		var data = history.urls[history.index].data;
 		getPage(li, domain, url, data, true);
+
 	}
 }
